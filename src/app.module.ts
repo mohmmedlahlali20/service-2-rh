d2ad99af4bb46b1423510529b081e04e0903f7f4
mohmmedlahlali20 <mohmmedlaeh81@gmail.com>
2024-12-19 09:18:14 +0100

    refactor: mise à jour de l'intégration des modules dans app.module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,15 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { MongooseModule } from '@nestjs/mongoose';
 import { RhModule } from './rh/rh.module';
 
 @Module({
   imports: [
-    
+    MongooseModule.forRoot(process.env.Mongo_URI || 'mongodb://localhost:27017/RH'),
     RhModule
   ],
+  
   controllers: [AppController],
   providers: [AppService],
 })
