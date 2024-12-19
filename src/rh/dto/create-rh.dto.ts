e97382c3a367f035df5ae0290b12fcd4ca9527b2
mohmmedlahlali20 <mohmmedlaeh81@gmail.com>
2024-12-19 09:18:20 +0100

    feat: ajout de nouvelles propriétés dans create-rh.dto

--- src/rh/dto/create-rh.dto.ts
+++ src/rh/dto/create-rh.dto.ts
@@ -1,1 +1,40 @@
-export class CreateRhDto {}
+import 
+{ 
+    IsEmail, 
+    IsNotEmpty, 
+    Length, 
+    MaxLength, 
+    IsNumber, 
+    Min 
+} from "class-validator";
+
+export class CreateRhDto {
+    @IsNotEmpty()
+    first_name: string;
+
+    @IsNotEmpty()
+    last_name: string;
+
+    @IsEmail()
+    @IsNotEmpty()
+    email: string;
+
+    @IsNotEmpty()
+    @Length(10, 10, { message: "Phone number must be exactly 10 digits long." })
+    phone_number: string; 
+
+    @IsNotEmpty()
+    date: string;
+
+    @IsNotEmpty()
+    @IsNumber()
+    @Min(0, { message: "Salary must be a positive number." })
+    salary: number;
+
+    @IsNotEmpty()
+    position: string
+    @IsNotEmpty()
+    department: string;
+
+
+}
