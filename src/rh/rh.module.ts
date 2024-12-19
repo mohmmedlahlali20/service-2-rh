d0411988cf20f462326ccf2a5154d1834a770d78
mohmmedlahlali20 <mohmmedlaeh81@gmail.com>
2024-12-19 09:18:23 +0100

    refactor: amélioration de la gestion des dépendances dans rh.module

--- src/rh/rh.module.ts
+++ src/rh/rh.module.ts
@@ -1,8 +1,13 @@
 import { Module } from '@nestjs/common';
 import { RhService } from './rh.service';
 import { RhController } from './rh.controller';
+import { MongooseModule } from '@nestjs/mongoose';
+import { RH, RHSchema } from './schema/RH.schema';
 
 @Module({
+  imports: [
+    MongooseModule.forFeature([{ name: RH.name, schema: RHSchema }])
+  ],
   controllers: [RhController],
   providers: [RhService],
 })
